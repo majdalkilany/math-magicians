221cc27f2f7a202de2235c3442ad33513a7099ff
majdalkilany <majdkilany92@gmail.com>
2023-07-12 12:21:51 +0300

    complete the calculator UI

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,3 +1,6 @@
+import './calculator.css';
+
+import { useState } from 'react';
 import Button from '../Button/Button';
 
 const buttons = [
@@ -23,11 +26,30 @@ const buttons = [
 ];
 
 const Calculator = () => {
-  const renderButtons = () => {
-    return buttons.map((btn) => {
-      return <button>{btn}</button>;
-    });
+  const [calculatorInputs, setCalculatorInputs] = useState('0');
+  const handleClick = (e) => {
+    const { value } = e.target;
+    let newCalculatorInputs = calculatorInputs;
+    if (calculatorInputs[0] === '0') {
+      newCalculatorInputs = calculatorInputs.slice(1);
+      setCalculatorInputs(newCalculatorInputs);
+    }
+    if (value === 'AC') {
+      setCalculatorInputs('0');
+    } else {
+      setCalculatorInputs(newCalculatorInputs + value);
+    }
   };
+  const renderButtons = buttons.map((btn, index) => (
+    <Button key={`${btn}-${index}`} btnValue={btn} handleClick={handleClick} />
+  ));
+
+  return (
+    <div className="calculator">
+      <input type="text" className="display" readOnly value={calculatorInputs} />
+      {renderButtons}
+    </div>
+  );
 };
 
 export default Calculator;
